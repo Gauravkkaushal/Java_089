479b85e68d515b5852883ec51e07fd88f049af6c
Gauravkkaushal <gaurav457th@gmail.com>
2025-08-26 11:14:07 +0530

    26 Aug 2025

---
+++ src/MyPersonMain.java
@@ -0,0 +1,9 @@
+public class MyPersonMain {
+    public static void main(String[] args) {
+        Person p=new Person("Ghaziabad");
+        p.getAddress();
+        SchoolStudent sc=new SchoolStudent("Sultanpur","B-tech");
+        sc.getAddress();
+        sc.getCourse();
+    }
+}
